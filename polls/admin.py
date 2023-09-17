6e25e9e964ffbc79ec94eba1879e3f8b120bf90e
wadie998 <wadierh2@gmail.com>
2023-09-17 02:04:01 -1200

    First commit

--- polls/admin.py
+++ polls/admin.py
@@ -2,4 +2,14 @@ from django.contrib import admin
 
 from .models import Question
 
-admin.site.register(Question)
+
+def make_published(modeladmin, request, queryset):
+    queryset.update(status='p')
+make_published.short_description = "Mark selected stories as published"
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ['question_text', 'status']
+    ordering = ['question_text']
+    actions = [make_published]
+
+admin.site.register(Question, QuestionAdmin)
